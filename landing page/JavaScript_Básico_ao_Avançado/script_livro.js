71208445c4a69760fa3f5ee691b2a5ef48aef3a0
Thomaz Rodrigues <112140581+Thomaz980@users.noreply.github.com>
2023-03-12 20:56:38 -0300

    Delete landing page/JavaScript_Básico_ao_Avançado directory

--- landing page/JavaScript_Básico_ao_Avançado/script_livro.js
+++
@@ -1,9 +0,0 @@
-var animaisMarinhos = ['peixe', 'baleia', 'golfinho', 'tubarão', 'lula'];
- for (var i = 0; i < animaisMarinhos.length; i++) {
- if (animaisMarinhos[i] == 'tubarão') {
- console.log(animaisMarinhos[i] + ' encontrado.');
- break;
- } else {
- console.log('Procurar animal marinho.');
- }
- }
